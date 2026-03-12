bd303012755dd4613534577bb3533e4251358036
vanloc19 <tovanloc19@gmail.com>
2026-03-13 02:05:13 +0700

    refactor(frontend): reorganize app entry and alias imports

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      Xin chào
+    </>
+  )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
